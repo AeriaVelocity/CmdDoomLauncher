b5d730b7a14a28924b2c8a4980cc8201ca6e10a2
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-28 14:36:16 +0100

    Add comments to build scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+rem CDL Build Script
+rem Change 'cdl' to change the executable name
 echo Building CDL with GCC...
 gcc -Wall -o cdl.exe main.c
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+# CDL Build Script
+# Change 'cdl' to change the executable name
 echo Building CDL with GCC...
 gcc -Wall -o cdl main.c
